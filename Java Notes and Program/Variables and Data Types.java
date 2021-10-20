f5a8b89d4cb424ca6d858e7fa8a3bb58a9b2538c
Sanjeev Kumar Prajapati <69379895+SanjeevKumarPrajapati@users.noreply.github.com>
2021-10-21 00:05:05 +0530

    Add files via upload

---
+++ Java Notes and Program/Variables and Data Types.java
@@ -0,0 +1,66 @@
+//WAP in java addition of 2 numbers
+import java.util.Scanner;
+public class a{
+	/*
+	static c=20;//static variable
+	void meth1() {
+		int b=20;//instance variable;
+	}
+	*/
+	public static void main(String args[]) {
+		//int a=10;//local variable
+		//Data type declearation
+		/*
+		boolean only=false;
+		System.out.println("Boolean data Type :-"+only);
+		byte ab=10;
+		System.out.println("Byte data Type :-"+ab);
+		short supply=1000;
+		System.out.println("Short data Type :-"+supply);
+		int a=20;
+		System.out.println("Int data Type :-"+a);
+		long aa=10000L;
+		System.out.println("Long data Type :-"+aa);
+		float f=234.5f;
+		System.out.println("Float data Type :-"+f);
+		double dy=12.3;
+		System.out.println("Double data Type :-"+only);
+		*/
+		//Taking input from the user;
+		/*
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Ente Your First Number :- ");
+		int a=obj.nextInt();
+		System.out.println("Enter Your Second Number :-");
+		int b=obj.nextInt();
+		int c=a+b;
+		System.out.println("My Addition is :- "+c);
+		*/
+		//Arithmetic Operators
+		//int a=10;
+		//int b=20;
+		//System.out.println("Arithmetic Operator(+) :- "+(a+b));
+		//Assignment Operators
+		/*
+		int aa=5;
+		int 
+		//+=,+=5=== a=a+5
+		System.out.println("Assignment Operators(+=) :- "+(++aa));
+		*/
+		//comparison
+		int a=10;
+		int b=10;
+		System.out.println("Comparison Operator(+) :- "+(a==b));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
