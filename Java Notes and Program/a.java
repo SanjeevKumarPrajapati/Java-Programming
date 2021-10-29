25babc36a0364c1aae188facaf2c845de4f31559
Sanjeev Kumar Prajapati <69379895+SanjeevKumarPrajapati@users.noreply.github.com>
2021-10-29 16:38:07 +0530

    Add files via upload

---
+++ Java Notes and Program/a.java
@@ -0,0 +1,108 @@
+import java.util.Scanner;
+public class a{
+	public static void main(String args[]) {
+		/*if(condition){
+		 *statement
+		 *}
+		 *else if(condition){
+		 *statement
+		 *}
+		 *else{
+		 *statement
+		 *}
+		 */
+		// WAP of grading system for fail,D grade,C grade,B grade,A grade and A+ grade
+		/*
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter Your Percentage :- ");
+		int a=sc.nextInt(); 
+		if(a<45){
+		    System.out.println("Fail");
+		}
+		else if(a>=45 && a<55){
+		    System.out.print("D Grade");
+		}
+		else if(a>=55 && a<65){
+		    System.out.print("C Grade");
+		}
+		else if(a>=65 && a<75){
+		    System.out.print("B Grade");
+		}
+		else if(a>=75 && a<90){
+		    System.out.print("A Garde");
+		}
+		else{
+		    System.out.print("A+ Grade");
+		}*/
+		//WAP to find given number is positive or negative (if, else if, else)(taking input from the user)
+		// Online Java Compiler
+		// Use this editor to write, compile and run your Java code online
+		/*
+		import java.util.Scanner;
+		class HelloWorld {
+		    public static void main(String[] args) {
+		        Scanner sc=new Scanner(System.in);
+				System.out.print("Enter Any Number :- ");
+				int a=sc.nextInt();
+				if(a>0){
+				    System.out.println("Number is Positive");
+				}
+				else if(a<0){
+				    System.out.println("Number is Negative");
+				}
+				else{
+				    System.out.println("neighter positive nore negative");
+				}
+		    }
+		}
+		*/
+		/*if (condition){
+		 *statement
+		 *if(condition){
+		 *statement}
+		 *}
+		 *else{
+		 *statement
+		 *}
+		 */
+		//
+		/*
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter Your Age :- ");
+		int a =sc.nextInt();
+		System.out.print("Enter Your Weight :- ");
+		int b=sc.nextInt();
+		if(a>=18){
+		    if(b>=50){
+		        System.out.println("You are eligible to donate blood..");
+		    }
+		    else{
+		        System.out.println("You are not eligible to donate blood..");
+		    }
+    }
+    else{
+        System.out.println("Age and weight must be greater and equal to 18 and 50");
+    }
+    }
+	*/
+	//WAP to find greatest no between three elements.
+		/*
+		 *  Scanner sc=new Scanner(System.in);
+		System.out.print("Enter Your first number :- ");
+		int a =sc.nextInt();
+		System.out.print("Enter Your Second Number :- ");
+		int b=sc.nextInt();
+		System.out.print("Enter Your Third Number :- ");
+		int c=sc.nextInt();
+		if(a>b&&a>c){
+		    System.out.println("First Number is Greater than All Numbers");
+		}
+		else if(b>a&&b>c){
+		    System.out.println("Second Numberis Greater than All numbers");
+		}
+		else{
+		    System.out.println("Third Number is Greater than All number");
+		}
+		 */
+	}
+}
